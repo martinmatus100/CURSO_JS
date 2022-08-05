d7914c9538ad097768a3d4ebb8960a525de24e8e
martinmatus100 <martinmatus100@gmail.com>
2022-08-05 01:01:37 -0300

    SIMULADOR INTERACTIVO: Piedra, Papel o Tijera [Martin]

--- js/main.js
+++ js/main.js
@@ -1,25 +1,105 @@
 /// VARIABLES
-const anioActual = 2022;
-let cantidadPersonas = prompt('Ingrese la cantidad de personas:');
-var edadPersona = "";
-var anioNacimiento = 0;
+let numGames = 0;
 
-/// CICLO 
-for (let i = 1; i <= cantidadPersonas; i++) {
-    anioNacimiento = 0;
-    edadPersona = prompt("Ingrese la edad de la persona " + i + ":");
+/// CICLOS
+//WHILE
+do {
+  numGames = prompt("Ingrese la cantidad de partidas a jugar (numero impar)");
+} while (!isOdd(numGames));
 
-    if (edadPersona != "") {
-        anioNacimiento = anioActual - edadPersona;
+//FOR
+for (let i = 1; i <= numGames; i++) {
+  // JUGADA JUGADOR
+  movePlayer = prompt("Ingrese Piedra [1], Papel [2] ó Tijera [3]: ");
+//   console.log("Eligio " + moveToText(movePlayer));
+  // JUGADA MAQUINA
+  moveIA = randomMove();
+//   console.log("Eligio " + moveToText(moveIA));
 
-        if (anioNacimiento < 0) {
-            alert("ERROR! La edad ingresada es muy alta.");
-            break;
-        }
+  //RESULTADOS
+  console.log('Partida ' + i + ': Jugador sacó [' + moveToText(movePlayer) + '] vs Maquina sacó [' + moveToText(moveIA) + ']');
+  console.log('Resultado: ' + gameResult(movePlayer, moveIA) + '!');
+}
 
-        alert("La persona " + i + " nació en el año " + anioNacimiento);
-    } else {
-        alert("ERROR! Ingrese una edad valida para la persona " + i);
-        break;
+/// FUNCIONES
+function isOdd(val) {
+  if (val % 2 == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function moveToText(move) {
+  switch (move) {
+    case "1":
+      return "Piedra";
+      break;
+
+    case "2":
+      return "Papel";
+      break;
+
+    case "3":
+      return "Tijera";
+      break;
+
+    case 1:
+      return "Piedra";
+      break;
+
+    case 2:
+      return "Papel";
+      break;
+
+    case 3:
+      return "Tijera";
+      break;
+  }
+}
+
+function randomMove() {
+  const min = 1;
+  const max = 3;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function gameResult(move1, move2) {
+    const rock = "Piedra";
+    const paper = "Papel";
+    const scissors = "Tijera";
+    const draw = "EMPATE";
+    const win = "GANASTE";
+    const lose = "PERDISTE"
+
+    let valMovePlayer = moveToText(move1);
+    let valMoveIA = moveToText(move2);
+
+    if (valMovePlayer === valMoveIA) {
+        return draw;
+    }
+
+    if (valMovePlayer === rock && valMoveIA === paper) {
+        return lose;
+    }
+
+    if (valMovePlayer === rock && valMoveIA === scissors) {
+        return win;
+    }
+
+    if (valMovePlayer === paper && valMoveIA === rock) {
+        return win;
+    }
+
+    if (valMovePlayer === paper && valMoveIA === scissors) {
+        return lose;
+    }
+
+    if (valMovePlayer === scissors && valMoveIA === rock) {
+        return lose;
+    }
+
+    if (valMovePlayer === scissors && valMoveIA === paper) {
+        return win;
     }
 }
