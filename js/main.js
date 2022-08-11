c5e3086ae0eb57dbce87d786b2719f01dabf0159
martinmatus100 <martinmatus100@gmail.com>
2022-08-11 00:51:13 -0300

    SIMULADOR-INTERACTIVO: Desafío: Incorporar Arrays [Martin]

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /// VARIABLES
 let numGames = 0;
+let gameResult = [];
 
 /// CICLOS
 //WHILE
@@ -11,16 +12,27 @@ do {
 for (let i = 1; i <= numGames; i++) {
   // JUGADA JUGADOR
   movePlayer = prompt("Ingrese Piedra [1], Papel [2] ó Tijera [3]: ");
-//   console.log("Eligio " + moveToText(movePlayer));
+  // console.log("Eligio " + moveToText(movePlayer));
   // JUGADA MAQUINA
   moveIA = randomMove();
-//   console.log("Eligio " + moveToText(moveIA));
+  // console.log("Eligio " + moveToText(moveIA));
 
   //RESULTADOS
-  alert('Partida ' + i + ': Jugador sacó [' + moveToText(movePlayer) + '] vs Maquina sacó [' + moveToText(moveIA) + ']\n Resultado: ' + gameResult(movePlayer, moveIA) + '!');
+  alert('Partida ' + i + ': Jugador sacó [' + moveToText(movePlayer) + '] vs Maquina sacó [' + moveToText(moveIA) + ']\n Resultado: ' + getResult(movePlayer, moveIA) + '!');
+  
   console.log('Partida ' + i + ': Jugador sacó [' + moveToText(movePlayer) + '] vs Maquina sacó [' + moveToText(moveIA) + ']');
-  console.log('Resultado: ' + gameResult(movePlayer, moveIA) + '!');
+  console.log('Resultado: ' + getResult(movePlayer, moveIA) + '!');
+
+  if (getResult(movePlayer, moveIA) == "EMPATE") {
+    i--;
+  } else {
+    gameResult.push(getResult(movePlayer, moveIA));
+  }
 }
+
+alert('RESULTADO FINAL: [' + gameResult + ']\n ' + getFinalResult(gameResult));
+console.log("RESULTADO FINAL: [" + gameResult + "]");
+console.log(getFinalResult(gameResult));
 
 /// FUNCIONES
 function isOdd(val) {
@@ -65,7 +77,7 @@ function randomMove() {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function gameResult(move1, move2) {
+function getResult(move1, move2) {
     const rock = "Piedra";
     const paper = "Papel";
     const scissors = "Tijera";
@@ -103,4 +115,25 @@ function gameResult(move1, move2) {
     if (valMovePlayer === scissors && valMoveIA === paper) {
         return win;
     }
+}
+
+function getFinalResult(result) {
+  let auxWin = 0;
+  let auxLose = 0;
+
+  for (let i = 0; i <= result.length; i++) {
+    if (result[i] == "GANASTE") {
+      auxWin++;
+    }
+
+    if (result[i] == "PERDISTE") {
+      auxLose++;
+    }
+  }
+
+  if(auxWin > auxLose) {
+    return "FELICITACIONES! GANASTE EL JUEGO"; 
+  } else {
+    return "MALA SUERTE! PERDISTE EL JUEGO";
+  }
 }
